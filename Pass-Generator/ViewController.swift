7b739134afa379b553da9f7790b24f9090647fe8
NostalgiaBox <rfenster@gmail.com>
2018-03-26 23:28:09 -0400

    Initial Commit

---
+++ Pass-Generator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pass-Generator
+//
+//  Created by Murray Fenstermaker on 3/26/18.
+//  Copyright © 2018 Nostalgiabox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
